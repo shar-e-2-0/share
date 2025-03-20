6b23ecda56c404225df16e2243649f50a60d7003
NiklasKnoerk <niklas.knoerk@edu.fhdw.de>
2025-03-20 22:59:09 +0100

    adding requirements for google maps for iOS SDK (without GMSApiKEy because some issues with the .gitignore)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps  // Google Maps SDK importieren
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,14 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // API-Key aus der Info.plist lesen
+    if let apiKey = Bundle.main.object(forInfoDictionaryKey: "GMSApiKey") as? String {
+      GMSServices.provideAPIKey(apiKey)
+    } else {
+      fatalError("Kein Google Maps API-Key in Info.plist gefunden!")
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
